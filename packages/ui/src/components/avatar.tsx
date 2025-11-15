09cab98b7c78c6ffa0276b32c1095a96d995f668
Huy <laiquanghuy24122001@gmail.com>
2025-11-15 13:48:47 +0700

    .

---
+++ packages/ui/src/components/avatar.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import type * as React from 'react'
+
+import { cn } from '@workspace/ui/lib/utils'
+import { Avatar as AvatarPrimitive } from 'radix-ui'
+
+const Avatar = ({ className, ...props }: React.ComponentProps<typeof AvatarPrimitive.Root>) => (
+    <AvatarPrimitive.Root
+      className={cn('relative flex size-8 shrink-0 overflow-hidden rounded-full', className)}
+      data-slot='avatar'
+      {...props}
+    />
+  ),
+  AvatarImage = ({ className, ...props }: React.ComponentProps<typeof AvatarPrimitive.Image>) => (
+    <AvatarPrimitive.Image className={cn('aspect-square size-full', className)} data-slot='avatar-image' {...props} />
+  ),
+  AvatarFallback = ({ className, ...props }: React.ComponentProps<typeof AvatarPrimitive.Fallback>) => (
+    <AvatarPrimitive.Fallback
+      className={cn('flex size-full items-center justify-center rounded-full bg-muted', className)}
+      data-slot='avatar-fallback'
+      {...props}
+    />
+  )
+
+export { Avatar, AvatarFallback, AvatarImage }
