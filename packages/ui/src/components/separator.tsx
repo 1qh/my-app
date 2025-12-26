41a24c7312afd0a09a3de3a179127ee560c698c8
Huy <laiquanghuy24122001@gmail.com>
2025-12-26 16:18:18 +0700

    .

---
+++ packages/ui/src/components/separator.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import type * as React from 'react'
+
+import { cn } from '@workspace/ui/lib/utils'
+import { Separator as SeparatorPrimitive } from 'radix-ui'
+
+const Separator = ({
+  className,
+  decorative = true,
+  orientation = 'horizontal',
+  ...props
+}: React.ComponentProps<typeof SeparatorPrimitive.Root>) => (
+  <SeparatorPrimitive.Root
+    className={cn(
+      'shrink-0 bg-border data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px',
+      className
+    )}
+    data-slot='separator'
+    decorative={decorative}
+    orientation={orientation}
+    {...props}
+  />
+)
+
+export { Separator }
